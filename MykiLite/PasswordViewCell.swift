25673315d732143bcf7f2096de49bc2d00093900
Ali Fakih <ali.fakih@x-tends.com>
2019-10-22 19:46:46 +0300

    create password view cell

---
+++ MykiLite/PasswordViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PasswordViewCell.swift
+//  MykiLite
+//
+//  Created by Ali Fakih on 10/22/19.
+//  Copyright © 2019 myki. All rights reserved.
+//
+
+import UIKit
+
+class PasswordViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
