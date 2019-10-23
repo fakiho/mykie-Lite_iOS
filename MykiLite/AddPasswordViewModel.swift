222049c4b69b3864e636d7d8aea28d65074549c9
Ali Fakih <ali.fakih@x-tends.com>
2019-10-23 17:04:33 +0300

    setup filtering by uuid

--- MykiLite/AddPasswordViewModel.swift
+++ MykiLite/AddPasswordViewModel.swift
@@ -7,12 +7,35 @@
 //
 
 import Foundation
+enum Fields: String {
+    case nickName = "NickName"
+    case userName = "UserName | Email"
+    case password = "Password"
+    case website = "Website"
+    case header = "Header"
+    
+    func getKey() -> String {
+        switch self {
+        
+        case .nickName:
+            return "nickname"
+        case .userName:
+            return "username"
+        case .password:
+            return "password"
+        case .website:
+            return "website"
+        case .header:
+            return ""
+        }
+    }
+}
 
 class AddPasswordViewModel {
 
   var password: Password!
   var fields: [Field] = []
-
+    var uuid: String?
   var numberOfCells: Int {
     return fields.count
   }
@@ -22,16 +45,29 @@ class AddPasswordViewModel {
   }
 
   func prepareFields() {
-    let items: [NSDictionary] = [["title": "Header", "type": FieldTypes.header, "secure": false], ["title": "Nickname", "type": FieldTypes.text, "secure": false], ["title": "Username | Email", "type": FieldTypes.text, "secure": false], ["title": "Password", "type": FieldTypes.password, "secure": true], ["title": "Website", "type": FieldTypes.text, "secure": false]]
+    let items: [NSDictionary] = [["title": Fields.header, "type": FieldTypes.header, "secure": false], ["title": Fields.nickName, "type": FieldTypes.text, "secure": false], ["title": Fields.userName, "type": FieldTypes.text, "secure": false], ["title": Fields.password, "type": FieldTypes.password, "secure": true], ["title": Fields.website, "type": FieldTypes.text, "secure": false]]
 
     for item in items {
-      guard let title = item["title"] as? String, let type = item["type"] as? FieldTypes, let isSecure = item["secure"] as? Bool else { continue }
+      guard let title = item["title"] as? Fields, let type = item["type"] as? FieldTypes, let isSecure = item["secure"] as? Bool else { continue }
       fields.append(Field(title: title, type: type, isSecure: isSecure))
     }
+    guard uuid != nil else {
+        return
+    }
+    
+    getPassword()
   }
 
   func updatePassword(item: NSDictionary) {
     password = Password(object: item)
   }
+    
+    func getPassword() {
+        print(database.fetch(with: getByUUID(uuid: uuid ?? "")))
+    }
+    
+    func getByUUID(uuid: String) -> FetchRequest<[Password], PasswordObject> {
+           return FetchRequest<[Password], PasswordObject>(predicate: NSPredicate(format: "uuid = %@", uuid), sortDescriptors: [], transformer: { $0.map(Password.init) })
+       }
 
 }
